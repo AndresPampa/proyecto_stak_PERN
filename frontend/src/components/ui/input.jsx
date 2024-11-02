eab5b1f46b3218b23015c09b9a741be3cbb9ddde
Andres Abdala Alvarez <andrespampa@gmail.com>
2024-11-02 20:09:29 -0300

    Terminando Stack PERN

--- frontend/src/components/ui/input.jsx
+++ frontend/src/components/ui/input.jsx
@@ -2,10 +2,9 @@ import {forwardRef} from 'react'
 
 export const Input = forwardRef((props, ref) => {
   return (
-    <input type="text" className='bg-zinc-800 px-3 py-2 block my-2 w-full' 
+    <input type="text" className="bg-zinc-800 px-3 py-2 block my-2 w-full" 
     {...props} ref={ref}/>
   )
-});
+})
 
-// Añadir displayName al componente
-Input.displayName = 'Input';
+export default Input
--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,58 +1,62 @@
-// import Input from "../components/ui/input";
-// import {Card} from "../components/ui/card";
-// import {Button} from "../components/ui/Button";
-// import { json, response } from "express";
-import {Button, Card, Input, Label} from "../components/ui"
-import {useForm} from "react-hook-form";
-import axios from "axios"
-import { Link } from "react-router-dom";
+import { Button, Card, Container, Input, Label } from "../components/ui";
+import  { useForm } from "react-hook-form";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
 
 function RegisterPage() {
+  
+  const { register, handleSubmit, formState: {errors} } = useForm();
 
-  const {register, handleSubmit, formState: {errors}} = useForm();
-
-  const onSubmit = handleSubmit(async(data) =>{
-      const res = await axios.post("http://localhost:3000/api/signup", data, {
-        withCredentials: true,
-      });
-      console.log(res);
+  const {signup, errors: setUserErrors } = useAuth();
+  const navigate = useNavigate();
+  const onSubmit = handleSubmit(async(data) => { 
+    const user = await signup(data);
+    if(user){
+      navigate("/tareas");
+    }
   });
 
+
   return (
-    <div className="h-[calc(100vh-64px)] flex items-center justify-center">
-      
+    <Container className="h-[calc(100vh-10rem)] flex items-center justify-center">
       <Card>
-        <h3 className= "text-4xl font-bold my-2">Registro</h3>
+        {setUserErrors &&
+          setUserErrors.map((error) => (
+            <p className="bg-red-500 text-white p-2">{error}</p>
+          ))}
+        <h3 className="text-4xl font-bold my-2">Registro</h3>
         <form onSubmit={onSubmit}>
           <Label htmlFor="name">Nombre</Label>
           <Input placeholder="Ingrese su nombre"
           {...register("name", {required:true})}></Input>
 
-          {
-            errors.name && <p className="text-red-500">Este Campo es requerido</p>
-          }
+        {
+          errors.name && <p className="text-red-500">Este campo es requerido</p>
+        }
           <Label htmlFor="email">Email</Label>
-          <Input type="email" placeholder="Ingrese su Email"
+          <Input type="email" placeholder="Ingrese su email"
           {...register("email", {required:true})}></Input>
 
-          {
-            errors.email && <p className="text-red-500">Este Campo es requerido</p>
-          }
-          <Label htmlFor="contraseña">Contraseña</Label>
-          <Input type="password" placeholder="Ingrese su Contraseña"
+        {
+          errors.email && <p className="text-red-500">Este campo es requerido</p>
+        }
+          <Label htmlFor="password">Contraseña</Label>
+          <Input type="password" placeholder="Ingrese su contraseña"
           {...register("password", {required:true})}></Input>
-          {
-            errors.password && <p className="text-red-500">Este Campo es requerido</p>
-          }
+
+        {
+          errors.password && <p className="text-red-500">Este campo es requerido</p>
+        }
           <Button>Registrarse</Button>
-      </form>
-      <div className="flex justify-between my-4 gap-1">
-            <p>¿Ya Tienes una Cuenta?</p>
-            <Link to="/login">Iniciar Sesion</Link>
+        </form>
+        <div className=" flex justify-between my-4">
+          <p className="mr-4">¿Ya tienes cuenta?</p>
+          <Link to="/login">Inciar Sesión</Link>
         </div>
       </Card>
-    </div>
-  )
+    </Container>
+  );
 }
 
-export default RegisterPage
+export default RegisterPage;
